8b17f471e6e82aec1cc8a4a8dfbcea8fa4a4947c
AlexandrPopo <70711466+AlexandrPopo@users.noreply.github.com>
2020-10-12 15:09:45 +0300

    Create task2.6.c

---
+++ task2.6.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main(){
+    /* 
+     E1 op= E2 эквивалентны E1 = E1 op E2
+     E1 op= E2 эквивалентны E1 = E1 op (E2)
+     */
+    int a,c;
+    a = 1;
+    c = 1;
+    a += c;
+    printf("%d",a);
+    a = a + (c);
+    printf("%d",a);
+}
